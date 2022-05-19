0e6c498f13f55bb24c604af30bcc7e8782acc25f
Rhea Mehta <rheamehta@ip-192-168-2-35.ec2.internal>
2022-05-19 11:53:26 -0400

    content changes

--- resume/scripts/script.js
+++ resume/scripts/script.js
@@ -4,21 +4,19 @@ var i;
 function addRandomFact() {
   const emojis = {
     mountain: String.fromCodePoint(0x26F0),
-    fencing: String.fromCodePoint(0x1F93A),
     laptop: String.fromCodePoint(0x1F4BB),
     dog: String.fromCodePoint(0x1F436),
     sparkle: String.fromCodePoint(0x2728),
     sneezing: String.fromCodePoint(0x1F927),
     musicnotes: String.fromCodePoint(0x1F3B6)
   }
+
   const facts =
-      [`is based in Vancouver, BC ${emojis.mountain}`,
-       `enjoys fencing, hiking, and skiing! ${emojis.fencing}`,
-       `is a logistics coordinator for nwPlus, home to the largest hackathon in Western Canada ${emojis.laptop}`, 
-       `has a goldendoodle named Luna! ${emojis.dog}`,
-       `is passionate about empowering girls in tech ${emojis.sparkle}`,
-       `recently transferred to computer science ${emojis.sneezing}`,
-       `loves Studio Ghibli films and Joe Hisaishi pieces ${emojis.musicnotes}`];
+      [`is based in Markham, ON ${emojis.musicnotes}`,
+       `enjoys hiking, running and netflix! ${emojis.mountain}`,
+       `is a software engineer at StackAdapt ${emojis.laptop}`, 
+       `loves dogs but is allergic to them! ${emojis.dog}${emojis.sneezing}`,
+       `is passionate about empowering girls in tech ${emojis.sparkle}`];
 
   // Pick a random fact.
   const fact = facts[Math.floor(Math.random() * facts.length)];
